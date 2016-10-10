2fdd9d8951dff5a61871bfa1e4d941d41f2f116e
ks-nireak <kimsrour.sophalnireak@gmail.com>
2016-10-10 13:44:03 +0700

    filter and update

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -2,13 +2,27 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import { Tasks } from '../../api/tasks.js';
 import template from './todosList.html';
- 
+
 class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
+    this.hideCompleted = false;
     this.helpers({
       tasks() {
-        return Tasks.find({});
+        const selector = {};
+        if (this.getReactively('hideCompleted')) {
+          selector.checked = {
+            $ne: true
+          };
+        }
+        return Tasks.find(selector);
+      },
+      incompleteCount() {
+        return Tasks.find({
+          checked: {
+           $ne: true
+          }
+        }).count();
       }
     })
   }
@@ -19,8 +33,18 @@ class TodosListCtrl {
     });
     this.newTask = '';
   }
+  setChecked(task) {
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
+  removeTask(task) {
+    Tasks.remove(task._id);
+  }
 }
- 
+
 export default angular.module('todosList', [
   angularMeteor
 ]).component('todosList', {
